520e9f65c8b7bb705bc8f8209cac1ca85a9582e1
Gaurav0002 <gauravking918@gmail.com>
2024-10-22 14:20:14 +0530

    Hosting Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,9 @@ import { Provider } from 'react-redux';
 import { store } from '../store/store';
 import '../app/globals.css';
 import Layout from '../app/layout';
+import { AppProps } from 'next/app'; // Import AppProps type
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => { // Use AppProps for typing
   return (
     <Provider store={store}>
       <Layout>
